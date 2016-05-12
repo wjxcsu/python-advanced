38ce0573c329f1c93a9ceda7934d989d1da43a72
wjxcsu <wjxcsu123@163.com>
2016-05-12 23:23:42 +0800

    添加greet_me函数

--- argsANDkwargs.py
+++ argsANDkwargs.py
@@ -6,3 +6,7 @@ def test_var_args(f_arg,*args):
 	print 'first normal arg:',f_arg
 	for arg in args:
 		print 'another arg through *args:',arg
+#**kwargs 允许你将不定长度的键值对, 作为参数传递给一个函数。 #如果你想要在一个函数里处理带名字的参数, 你应该使用**kwargs。
+def greet_me(**kwargs):
+	for key,value in kwargs.items():
+		print '%s==%s'%(key,value)
